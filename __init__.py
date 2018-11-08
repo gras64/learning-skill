b1a649b77b79a0a5b750c8a70b378dcacd39491a
Andreas Reinle <gras64@web.de>
2018-11-08 20:33:22 +0100

    only first dialog

--- __init__.py
+++ __init__.py
@@ -1,23 +1,37 @@
+from adapt.intent import IntentBuilder
 from os.path import dirname
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler, intent_file_handler
+from mycroft.audio import wait_while_speaking
+from mycroft.util.log import LOG, getLogger
 
 _author__ = 'gras64'
-# Release - 000001
+LOGGER = getLogger(__name__)
 
 class Learning(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.local_path = ".private"
         self.public_path = ".public"
+        self.privacy = ""
 
+    def get_category_response(self, dialog):
+                while True:
+                    catego = self.get_response(dialog)
+                    #more of categorys enter here
+                    if catego == "humor.intent":
+                    elif catego == "science.intent":
+                    elif catego == "love.intent":
+                    else:
+                        self.speak_dialog("invalid.category")
 
     @intent_file_handler('learning.intent')
     def handle_learning(self, message):
         self.speak_dialog('learning')
 
     @intent_file_handler('private.intent')
-    def handle_answers_thebest(self, message):
+    def handle_private(self, message):
         self.speak_dialog('private')
+        category = get_category_response("get.lower")
 
 def create_skill():
     return Learning()
